14afbc419c9e99f37f8c5ec179dab066a665160a
khairy <khairymahmoud795@gmail.com>
2025-05-20 00:42:49 +0200

    handle form

---
+++ src/app/Form/page.tsx
@@ -0,0 +1,75 @@
+'use client'
+import React from 'react'
+import { useForm } from 'react-hook-form'
+
+
+type FormValues = {
+    username: string,
+    email: string,
+    channel: string,
+    address: {
+        line1: string,
+        line2: string
+    }
+}
+
+const defaultValues: FormValues =  {
+    username: "mahmoud",
+    email:"khairy@gmail.com",
+    channel: "youtube Channel",
+    address: {
+        line1: "assuit",
+        line2: "cairo"
+    }
+}
+const FormPage = () => {
+
+    const form = useForm<FormValues>({
+        defaultValues
+    });
+
+    const {register, handleSubmit, reset} = form;
+
+    const submitForm = (values: FormValues) => {
+        console.log('Data from form', values);
+    }
+
+    const onReset = () => {
+        reset(defaultValues);
+    };
+    return (
+        <div>
+            <h1>Learn web hook Form</h1>
+            <form onSubmit={handleSubmit(submitForm)}>
+                <div className="form-control">
+                    <label htmlFor="username">Username</label>
+                    <input type="text" id="username"  {...register('username')}/>
+                </div>
+                <div className="form-control">
+                    <label htmlFor="email">E-mail</label>
+                    <input type="email" id="email" {...register('email')} />
+                </div>
+                <div className="form-control">
+                    <label htmlFor="channel">Channel</label>
+                    <input type="text" id="channel" {...register('channel')}/>
+                </div>
+
+                <div className="form-control">
+                    <label htmlFor="address1">Address 1</label>
+                    <input type="text" id="address1" {...register('address.line1')} />
+                </div>
+
+                <div className="form-control">
+                    <label htmlFor="address2">Address 2</label>
+                    <input type="text" id="address2" {...register('address.line2')} />
+                </div>
+
+                <button type='submit'>Submit</button>
+                <button type='button' onClick={onReset}>
+                    Reset
+                </button>
+            </form>
+        </div>
+    )
+}
+export default FormPage
