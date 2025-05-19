eec390b16fab87872ea3df893e9bf9435b81bbed
khairy <khairymahmoud795@gmail.com>
2025-05-20 01:52:22 +0200

    handle button

--- src/app/Form/page.tsx
+++ src/app/Form/page.tsx
@@ -1,5 +1,5 @@
 'use client'
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useForm } from 'react-hook-form'
 
 
@@ -14,7 +14,7 @@ type FormValues = {
 }
 
 const defaultValues: FormValues =  {
-    username: "mahmoud",
+    username: "",
     email:"khairy@gmail.com",
     channel: "youtube Channel",
     address: {
@@ -28,7 +28,7 @@ const FormPage = () => {
         defaultValues
     });
 
-    const {register, handleSubmit, reset} = form;
+    const {register, handleSubmit, reset, formState, watch} = form;
 
     const submitForm = (values: FormValues) => {
         console.log('Data from form', values);
@@ -37,13 +37,36 @@ const FormPage = () => {
     const onReset = () => {
         reset(defaultValues);
     };
+
+    const {errors, isDirty, isValid} = formState
+    useEffect(() => {
+        const subscription = watch((value, { name, type }) => {
+            console.log(value, name, type)
+        });
+        return () => subscription.unsubscribe();
+    }, [watch]);
+
     return (
         <div>
             <h1>Learn web hook Form</h1>
             <form onSubmit={handleSubmit(submitForm)}>
                 <div className="form-control">
                     <label htmlFor="username">Username</label>
-                    <input type="text" id="username"  {...register('username')}/>
+                    <input type="text" id="username"  {...register('username', {
+                        required: {
+                            value: true,
+                            message: "userName is required"
+                        },
+                        validate: (value) => {
+                            if(value.length > 3){
+                                return true
+                            }else{
+                                return "Must be greater than 3"
+                            }
+                        }
+                        }
+                    )}/>
+                    <p className='error'>{errors?.username?.message}</p>
                 </div>
                 <div className="form-control">
                     <label htmlFor="email">E-mail</label>
@@ -64,7 +87,7 @@ const FormPage = () => {
                     <input type="text" id="address2" {...register('address.line2')} />
                 </div>
 
-                <button type='submit'>Submit</button>
+                <button type='submit' disabled={!isDirty || !isValid}>Submit</button>
                 <button type='button' onClick={onReset}>
                     Reset
                 </button>
